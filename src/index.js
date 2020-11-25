56c2d0ab6f248a023e6edfa44bee286ecdb601b5
markdenardo <mark.denardo@gmail.com>
2020-11-25 17:52:40 -0500

    exercise 1.4 solution

--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@ const Content = (props) => {
   console.log('Content props', props)
   return (
     <div>
-      <Part name = {props.part1} exercises = {props.exercises1}/>
-      <Part name={props.part2} exercises={props.exercises2}/>
-      <Part name={props.part3} exercises={props.exercises3}/>
+      <Part name = {props.name} exercises = {props.exercises}/>
+      {/* <Part name = {props.parts[1].name} exercises = {props.parts[1].exercises}/>
+      <Part name = {props.parts[2].name} exercises = {props.parts[2].exercises}/> */}
     </div>
   )
 }
@@ -31,18 +31,20 @@ const Part = (props) => {
 
 const App = () =>{
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return(
     <>
@@ -50,9 +52,9 @@ const App = () =>{
         <Header course={course} />
     </h1>
     <div>
-        <Content part1={part1.name} exercises1={part1.exercises}/>
-        <Content part2={part2.name} exercises2={part2.exercises}/>
-        <Content part3={part3.name} exercises3={part3.exercises}/>  
+        <Content name={parts[0].name} exercises={parts[0].exercises}/>
+        <Content name={parts[1].name} exercises={parts[1].exercises} />
+        <Content name={parts[2].name} exercises={parts[2].exercises} />
     </div>
     
     </>
